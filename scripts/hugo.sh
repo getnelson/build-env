4883e2d7f9e18bcc1e8ce8f69d618c8634f3588d
Timothy Perrett <tperrett@lyft.com>
2018-09-13 11:44:09 -0700

    Upgrade Hugo version

--- scripts/hugo.sh
+++ scripts/hugo.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.22.1/hugo_0.22.1_Linux-64bit.tar.gz -O /tmp/hugo.tgz
+HUGO_VERSION="0.48"
+
+wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" -O /tmp/hugo.tgz
 tar xvf /tmp/hugo.tgz -C /tmp
 mv /tmp/hugo /usr/local/bin
 chmod +x /usr/local/bin/hugo
